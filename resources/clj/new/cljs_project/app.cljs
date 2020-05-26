a1a954b228d95ccbea8f1d72b7a02ec57f90b92c
Edwin Park <edwin@factual.com>
2020-05-25 19:36:37 -0700

    refactored app example

--- resources/clj/new/cljs_project/app.cljs
+++ resources/clj/new/cljs_project/app.cljs
@@ -1,28 +1,26 @@
 (ns {{namespace}}.app
   (:require [helix.core :refer [defnc $]]
-            [helix.hooks :as hooks]
             [helix.dom :as d]
             ["react-dom" :as rdom]))
 
-(defnc greeting
-  "A component which greets a user."
-  [{:keys [name]}]
-  (d/div "Hello, " (d/strong name) "!"))
-
 (defnc app []
-  (let [[state set-state] (hooks/use-state {:name "Helix User"})]
-    (d/div
-     (d/h1 "Welcome!")
-     ($ greeting {:name (:name state)})
-     (d/input {:value (:name state)
-               :on-change #(set-state assoc :name (.. % -target -value))}))))
+  (d/div {:class ["h-screen"
+                  "flex"
+                  "justify-center"
+                  "items-center"
+                  "text-4xl"]}
+         "{{namespace}}"))
+
+(defn render []
+  (rdom/render ($ app) (js/document.getElementById "root")))
 
 (defn init! []
   (println "init!")
-  (rdom/render ($ app) (js/document.getElementById "root")))
+  (render))
 
 (defn ^:dev/before-load stop []
   (println "stop"))
 
 (defn ^:dev/after-load start []
-  (println "start"))
+  (println "start")
+  (render))
